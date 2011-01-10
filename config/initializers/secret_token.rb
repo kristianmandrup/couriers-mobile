dde7702dfeb2b127dad31dddec9795ad8d0f2b30
Kristian Mandrup <kmandrup@gmail.com>
2011-01-10 18:56:27 +0100

    initial config

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TiramizooCourierApp::Application.config.secret_token = 'b55816e4736dd02b44266531ed89f0783f937c14a0a5007995277525c7f0db52636e8b32d24f88e7b780405006c35ec2da95dfe9e8c231586b5ecc528ae94810'
