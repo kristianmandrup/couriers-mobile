5fae6ba7d0dec1b967cd2d9a78ceff4b6ca169c7
fe9lix <fe9lix@googlemail.com>
2011-01-12 01:36:06 +0100

    fixed: basic setup with some routes, controllers and haml views, added default application layout with jquery mobile

---
+++ app/controllers/delivery/state_controller.rb
@@ -0,0 +1,20 @@
+module Delivery
+
+  class StateController < ApplicationController
+
+    def index
+
+    end
+
+    def accept
+      
+    end
+
+    # method to switch state in state machine
+    def set_state
+
+    end
+
+  end
+
+end
---
+++ app/controllers/settings_controller.rb
@@ -0,0 +1,15 @@
+class SettingsController < ApplicationController
+
+  def show
+    
+  end
+
+  def edit
+    
+  end
+
+  def update
+    
+  end
+
+end
